a96318e43b0623f122126aceee8a445f46a985f3
lanjian <lanjian1006@gmail.com>
2022-05-23 17:22:49 +0800

    Import and export statement

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import DMenu, { DMenuItem, DSubMenu } from 'components/DMenu/index'
+import DTable from 'components/DTable/index'
+
+export {
+    DMenu,
+    DMenuItem,
+    DSubMenu,
+    DTable
+}
